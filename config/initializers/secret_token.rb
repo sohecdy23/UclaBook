c095a2783b5185ac6160fc6fe4d11f21242625ef
Sohecdy Avila <sohecdyavila38@gmail.com>
2017-01-28 21:11:58 -0430

    creado el proyecto y agregado status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UclaBook::Application.config.secret_token = 'cb53065b9494cbe3d473cfd9d14de831e1e00074864d311a3997ce376080f266bfdc760890f11a13ddcd2fbdda53a8656a6a5244ac143abccea0ca10b16dac8b'
